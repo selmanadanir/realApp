502e58ef1cf429842c63b73bf98a441e747c3222
selman adanir <adanirselman@gmail.com>
2022-07-25 16:45:11 +0300

    make design of app

---
+++ realApp/HomeTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  HomeTableViewCell.swift
+//  realApp
+//
+//  Created by Selman ADANİR on 25.07.2022.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var myImage: UIImageView!
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    
+    
+    
+    
+    
+}
--- realApp/ViewController.swift
+++ realApp/ViewController.swift
@@ -11,9 +11,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        loadLogo()
+        
     }
-
-
+    
+    func loadLogo(){
+        if let logo = UIImage(named: "the-new-york-times-logo.png"){
+            let newLogo = Util.app.resizeImage(image: logo, targetSize: CGSize(width: 200, height: 50))
+            let imageView = UIImageView(image: newLogo)
+            self.navigationItem.titleView = imageView
+            
+        }
+    }
 }
-
